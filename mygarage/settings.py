b356ad20455a35578ec751abaf242d818dc2e486
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-01-23 21:01:24 +0100

    edit

--- mygarage/settings.py
+++ mygarage/settings.py
@@ -135,8 +135,10 @@ LANGUAGES = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
