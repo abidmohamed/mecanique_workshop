c655a2a873c6c42c912509225fba5f822066135d
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-02-11 19:13:33 +0100

    first edition

---
+++ rdv/templates/rdv/customer_list.html.py
@@ -0,0 +1,70 @@
+BBBBBBB BBBBBBBBBBB
+BBBB BBBBBB
+BBBB BBBBBBBBBBBBBBBBB
+BBBBB BBBBBBB
+XXXX
+XXXX XXXXXXXXXXXX
+    XXXX XXXXXXXXXXXXXXX
+        XXXX XXXXXXXXXXX XXXXXXXXXXX
+            XXXXXXXXXXXXXXXXX
+        XXXXXX
+
+        XXXX XXXXXXXXXXX XXXXXXXXXXX
+            XXXXX XXXXXXXXX XXXXXXXXXXXXXX
+                BBBBBBBBBB
+                XXXXXXXXXX X XXXXXXXXXXXXX
+                XXXXXX XXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXX
+                    XXXXXXX
+                    XXXX
+                        XXXXXXXXX
+                        XXXXXXXXXXXXX
+                        XXXXXXXXXXXXXX
+                        XXXXXXXXXXXXXXXX
+                        XXXXXXXXXXXXXX
+
+                    XXXXX
+                    XXXXXXXX
+                    XXXXXXX
+                    BBB BBBBBBBB BB BBBBBBBBB
+                    XXXX
+                        XXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXX XXXXXXXX XXXXX
+                                   XXXXXXXXXXXXXXXX
+                        XXXXX
+                        XXXX
+                             
+                        XXXXX
+                        XXXX
+                            
+                        XXXXX
+                        XXXX
+                            
+                        XXXXX
+                        XXXX
+                            
+                        XXXXX
+                    XXXXX
+                    BBBBBB
+                    XXXXXXXX
+                XXXXXXXX
+                XXXX
+                BBBBBBBBBB
+                XXXXXX XXXXXXXXXX XXXXXXXXXXXX XXXXXXXXXXXXX XXXXXXXXXXXXXX
+            XXXXXXX
+        XXXXXX
+    XXXXXX
+XXXXXX
+BBBBBBBB
+BBBBB BBBBBBBB
+XXXX XXXX XXXXX  XXX
+XXXXX XXXXXXXXXXXXXXXX XXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+XXXXXXX XXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+XXXXXXX XXXXXXXXXXXXXXXXXXXXXXX
+    XXXXXX XXXXX XXXX
+    XXXXXXXXXXXXXXXXXX XXXXXXXX XX X
+    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    X XX
+
+XXXXXXXXX
+BBBBBBBB
