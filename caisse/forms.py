dfbf5b88fc58e7c5af36466cf3726920c3e48bb9
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-03-09 19:31:27 +0100

    transaction list

--- caisse/forms.py
+++ caisse/forms.py
@@ -23,3 +23,16 @@ class DateForm(forms.Form):
                                    'class': 'form-control datepicker', 'type': 'date'
                                })
                                )
+
+
+class PeriodForm(forms.Form):
+    start_date = forms.DateTimeField(initial="2021-01-01",
+                                     widget=forms.DateInput(attrs={
+                                         'class': 'form-control datepicker', 'type': 'date'
+                                     })
+                                     )
+    end_date = forms.DateTimeField(initial="2021-01-01",
+                                   widget=forms.DateInput(attrs={
+                                       'class': 'form-control datepicker', 'type': 'date'
+                                   })
+                                   )
--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from payments.models import SellOrderPayment, BuyOrderPayment
+
 
 # Register your models here.
+@admin.register(SellOrderPayment)
+class SellOrderPaymentAdmin(admin.ModelAdmin):
+    list_display = ('order', 'customer', 'amount', 'created', 'pay_status')
+
+
+@admin.register(BuyOrderPayment)
+class BuyOrderPaymentAdmin(admin.ModelAdmin):
+    list_display = ('order', 'supplier', 'amount', 'created', 'pay_status')
