185445b4a0bcaaebba508e8e3c94b7c23352033e
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-03-13 12:59:44 +0100

    sell order admin

--- sellorder/admin.py
+++ sellorder/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from sellorder.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'created')
