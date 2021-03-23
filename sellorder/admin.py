a4bdbee2fbf872351276764ec6a249f9664eddd5
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-03-23 13:04:36 +0100

    sell order bill admin

--- sellorder/admin.py
+++ sellorder/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from sellorder.models import Order
+from sellorder.models import Order, SellOrderFacture
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'customer', 'created')
+
+
+@admin.register(SellOrderFacture)
+class BillOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'created')
