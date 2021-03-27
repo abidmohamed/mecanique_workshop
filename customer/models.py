7f61c4ec7eb0da6cec3dd9a41b64dd1778472236
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-03-27 19:18:15 +0100

    delete customer email & national number

--- customer/models.py
+++ customer/models.py
@@ -15,8 +15,8 @@ class Customer(models.Model):
     lastname = models.CharField(max_length=250, null=True)
     address = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
-    national_number = models.CharField(max_length=200, null=True)
-    email = models.EmailField(null=True)
+    # national_number = models.CharField(max_length=200, null=True)
+    # email = models.EmailField(null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     date_joined = models.DateField(null=True)
 
