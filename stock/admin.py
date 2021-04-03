ea5f0f2576a02d176d5a82bfbe20868cc4398a01
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-04-03 10:39:48 +0100

    stockproducts admin

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from stock.models import StockProduct
+
+
+@admin.register(StockProduct)
+class StockProductAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity'),
+    search_fields = 'product',
