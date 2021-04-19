1b2a0871e628ea054b34233e7c36fb3569f5c4db
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-04-19 16:49:10 +0100

    order pdf add remise

--- customer/models.py
+++ customer/models.py
@@ -18,6 +18,7 @@ class Customer(models.Model):
     # national_number = models.CharField(max_length=200, null=True)
     # email = models.EmailField(null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    date_updated = models.DateTimeField(auto_now=True)
     date_joined = models.DateField(null=True)
     enterprise = models.BooleanField(default=False)
     debt = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
