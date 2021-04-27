9d6c4357e31653ac3669df1b363fa06bc59c354d
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-04-27 13:25:55 +0100

    service & service provider

---
+++ services/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm, modelformset_factory
+from services.models import ServiceProvider, Service
+
+
+class ServiceProviderForm(ModelForm):
+    class Meta:
+        model = ServiceProvider
+
+        fields = '__all__'
+
+
+class ServiceForm(ModelForm):
+    class Meta:
+        model = Service
+
+        fields = '__all__'
