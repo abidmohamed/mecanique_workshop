d633b4d2f4a35e2e70501fb848a61b872147e6d6
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-08-11 07:58:46 +0100

    Delete set product to null on delete in buyorder and sell order

--- sellorder/models.py
+++ sellorder/models.py
@@ -58,7 +58,7 @@ class OrderItem(models.Model):
                               on_delete=models.CASCADE)
     stockproduct = models.ForeignKey(StockProduct,
                                      related_name='order_item',
-                                     on_delete=models.DO_NOTHING, null=True, blank=True)
+                                     on_delete=models.SET_NULL, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
