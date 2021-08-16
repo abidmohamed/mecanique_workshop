1d91bda4510f26b291b8f42614137e60fd92e09e
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-08-16 12:38:47 +0100

    ARABIC TO PDF

--- services/models.py
+++ services/models.py
@@ -7,6 +7,7 @@ class ServiceProvider(models.Model):
     phone = models.CharField(max_length=200, null=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    credit = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0.0)
 
     def __str__(self):
         return self.name
