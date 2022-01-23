c972841bda760829509c4219f844d60fd8e2822b
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2022-01-23 16:48:32 +0100

    changes of 23/01/2022

--- product/views.py
+++ product/views.py
@@ -101,16 +101,16 @@ def detail_product(request, pk):
     for order in all_sellorders:
         print("###### ORDER")
         for item in order.items.all():
-
-            if item.stockproduct.product.id == product.id:
-                print("####### ITEM FOUND")
-                print(item.stockproduct.product)
-                print(product)
-                chosen_orders |= Order.objects.all().filter(id=order.id)
-                pieces |= order.items.all().filter(id=item.id)
-                sell_quantity += item.quantity
-                quantities.append(item.quantity)
-                # print(order.items.all().filter(id=item.id))
+            if StockProduct.objects.filter(product=product):
+                if item.stockproduct.product.id == product.id:
+                    print("####### ITEM FOUND")
+                    print(item.stockproduct.product)
+                    print(product)
+                    chosen_orders |= Order.objects.all().filter(id=order.id)
+                    pieces |= order.items.all().filter(id=item.id)
+                    sell_quantity += item.quantity
+                    quantities.append(item.quantity)
+                    # print(order.items.all().filter(id=item.id))
     final_list = zip(chosen_orders, pieces, quantities)
 
     # Buy orders product count
