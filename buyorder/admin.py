5ada1bf5b3bd9554afd0711c6d571e9938b26e6c
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2022-02-23 15:35:42 +0100

    changes of 23/02/2022

--- buyorder/admin.py
+++ buyorder/admin.py
@@ -7,3 +7,4 @@ from buyorder.models import BuyOrder
 @admin.register(BuyOrder)
 class BuyorderAdmin(admin.ModelAdmin):
     list_display = ('id', 'supplier', 'created')
+    search_fields = ['supplier', ]
