26b1fe115cb1b2e8d342943829a3b1959c4ee3ec
abidmohamed <abidmohamednadhir@gmail.com>
2022-02-28 17:21:11 +0100

    Update settings.py

--- mygarage/settings.py
+++ mygarage/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-wx59#clvu0c72ph%aj2jeh-!(970fwt@b%w4fvdjga02$@gv2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
